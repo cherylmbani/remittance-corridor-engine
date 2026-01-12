701b454da1281d1de9c9634a69d832f49cfbfb88
Cheryl Mbani <mbanicheryl45@gmail.com>
2026-01-12 13:15:40 +0300

    Add signup, login, logout endpoints in app.py

--- server/app.py
+++ server/app.py
@@ -22,6 +22,54 @@ class Start(Resource):
         response={'message': 'Welcome to Remittance Corridor Engine!'}
         return response, 200
 
+#aAuthentication routes
+
+class Signup(Resource):
+    def post(self):
+
+        data=request.get_json()
+        existing_user=User.query.filter_by(email=data['email']).first()
+        if existing_user:
+            return {"error": " Email already exists!"}, 400
+        new_user=User(
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            email=data['email'],
+            password=data['password']
+
+        )
+        
+        db.session.add(new_user)
+        db.session.commit()
+        new_user_dict=new_user.to_dict()
+        response_body=make_response(new_user_dict, 201)
+        return response_body
+
+class Login(Resource):
+    def post(self):
+        data=request.get_json()
+        if not data:
+            return {'error': 'Please fill in all the fields'}
+        email=data['email']
+        password=data['password']
+        if not email or not password:
+            return {'error': 'Both email and password required'}, 404
+        user=User.query.filter_by(email=email).first()
+        if user and user.authenticate(password):
+            session['user_id']=user.id
+            user_dict=user.to_dict()
+            response=make_response(user_dict, 200)
+            return response
+        else:
+            response_body={"error":"Invalid email or password"}
+            return response_body, 400
+
+class Logout(Resource):
+    def post(self):
+        session.pop('user_id', None)
+        response_body={'message': "Logged out successfully!"}
+        response=make_response(response_body, 200)
+        return response
 
 class UserResource(Resource):
     def get(self):
@@ -314,6 +362,9 @@ class TransactionById(Resource):
 
 
 api.add_resource(Start, '/welcome')
+api.add_resource(Signup, '/signup')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
 api.add_resource(UserResource, '/users')
 api.add_resource(UserById, '/users/<int:id>')
 api.add_resource(CountryResource, '/countries')
@@ -331,4 +382,4 @@ api.add_resource(TransactionById, '/transactions/<int:id>')
 if __name__=='__main__':
     app.run(port=5555, debug=True)
 
-# kill -9$(lsof -t -i:5555)
+# kill -9 $(lsof -t -i:5555)
